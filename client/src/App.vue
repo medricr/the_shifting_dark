89059f439377653bbd73d4d6b38b295bd4d6d8ef
medricr <medricr@gmail.com>
2021-05-03 16:17:33 -0700

    file structure changes, added vue again. Issue is with Safari

---
+++ client/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Splashpage />
+  </div>
+</template>
+
+<script>
+import Splashpage from './components/Splashpage'
+
+export default {
+  name: 'App',
+  components: {
+    Splashpage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ client/src/components/Splashpage.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <h1>The Shifting Dark</h1>
+        <p>A single-shot adventure module for the Mothership RPG.</p>
+        <p>{{msg}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Splashpage",
+    props: {
+        msg: String
+    }
+}
+</script>
+
+<style>
+.container {
+    background-color: black;
+    color: white;
+}
+</style>
