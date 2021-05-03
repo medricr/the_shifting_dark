e59305895a8ba758c46a32923bc2bd941c8907bd
medricr <medricr@gmail.com>
2021-05-03 14:10:45 -0700

    file structure changes, deleted some default vue stuff

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("test");
+const express = require("express");
+const serveStatic = require("serve-static");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3030;
+
+app.use('/', serveStatic(path.join(__dirname, '/dist')));
+app.get(('/.*/', function(req,res){
+    res.sendFile(path.join(__dirname, '/dist/index.html'))
+}));
+
+app.listen(PORT, ()=> {
+    console.log(`App listening on port => ${PORT} 👀`)
+})
